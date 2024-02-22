cf2d33ff17e3bef229813c10bfb782ee3a566b0a
Pablo Sangenis <sangenispablo@gmail.com>
2024-02-21 23:02:29 -0300

    Se acepta y responde JSON implemento CR solamente

---
+++ src/services/workoutService.js
@@ -0,0 +1,48 @@
+const { v4: uuid } = require("uuid");
+
+const Workout = require("../database/Workout");
+
+const getAllWorkouts = () => {
+  const allWorkouts = Workout.getAllWorkouts();
+  return allWorkouts;
+};
+
+const getOneWorkout = () => {
+  return;
+};
+
+const createNewWorkout = (newWorkout) => {
+  // aca usamos el operador spread para expandir el JSON en la nueva variable
+  const workoutToInsert = {
+    ...newWorkout,
+    // genero el id
+    id: uuid(),
+    // cargo las fechas
+    // createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    // updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    createdAt: new Date().toLocaleString("es-AR", {
+      timeZone: "America/Argentina/Buenos_Aires",
+    }),
+    updatedAt: new Date().toLocaleString("es-AR", {
+      timeZone: "America/Argentina/Buenos_Aires",
+    }),
+  };
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+  return createdWorkout;
+};
+
+const updateOneWorkout = () => {
+  return;
+};
+
+const deleteOneWorkout = () => {
+  return;
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
